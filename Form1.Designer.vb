e87283f49cba422e357318aec22bc89a73506f56
kakksanthusa <41253333+kakksanthusa@users.noreply.github.com>
2019-10-26 22:41:50 +0530

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,94 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Dock = System.Windows.Forms.DockStyle.Left
+        Me.PictureBox2.Image = CType(resources.GetObject("PictureBox2.Image"), System.Drawing.Image)
+        Me.PictureBox2.Location = New System.Drawing.Point(0, 0)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(47, 40)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox2.TabIndex = 1
+        Me.PictureBox2.TabStop = False
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.Dock = System.Windows.Forms.DockStyle.Right
+        Me.PictureBox3.Image = CType(resources.GetObject("PictureBox3.Image"), System.Drawing.Image)
+        Me.PictureBox3.Location = New System.Drawing.Point(236, 0)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(44, 40)
+        Me.PictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox3.TabIndex = 2
+        Me.PictureBox3.TabStop = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Dock = System.Windows.Forms.DockStyle.Right
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(192, 0)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(44, 40)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 3
+        Me.PictureBox1.TabStop = False
+        '
+        'Form1
+        '
+        Me.AllowDrop = True
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(221, Byte), Integer), CType(CType(221, Byte), Integer), CType(CType(221, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(280, 40)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.PictureBox3)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.ShowInTaskbar = False
+        Me.Text = "Google"
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents PictureBox2 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox3 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,108 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.ShowInTaskbar = False
+        Me.FormBorderStyle = FormBorderStyle.None
+        Me.Location = New Point(My.Settings.locationx)
+        Me.Size = New Size(My.Settings.width)
+
+
+
+
+
+        borderchanger()
+
+
+    End Sub
+
+
+    Public Function borderchanger()
+        Me.FormBorderStyle = FormBorderStyle.None
+        Me.Height = 40
+
+        Dim p As New Drawing2D.GraphicsPath()
+        p.StartFigure()
+        p.AddArc(New Rectangle(0, 0, 40, 40), 180, 90)
+        p.AddLine(40, 0, Me.Width - 40, 0)
+        p.AddArc(New Rectangle(Me.Width - 40, 0, 40, 40), -90, 90)
+        p.AddLine(Me.Width, 40, Me.Width, Me.Height - 40)
+        p.AddArc(New Rectangle(Me.Width - 40, Me.Height - 40, 40, 40), 0, 90)
+        p.AddLine(Me.Width - 40, Me.Height, 40, Me.Height)
+        p.AddArc(New Rectangle(0, Me.Height - 40, 40, 40), 90, 90)
+        p.CloseFigure()
+        Me.Region = New Region(p)
+        Return (True)
+    End Function
+
+ 
+
+
+
+    
+
+    Dim changing As Boolean
+    Private Sub Form1_SizeChanged(sender As Object, e As EventArgs) Handles MyBase.SizeChanged, MyBase.LocationChanged
+        If Me.WindowState = FormWindowState.Maximized Then
+            Me.WindowState = FormWindowState.Normal
+        End If
+        If changing = True Then
+            borderchanger()
+            My.Settings.width = Me.Size
+            My.Settings.locationx = Me.Location
+            Try
+                System.Diagnostics.Process.Start(System.IO.Path.GetFullPath("restart.exe"))
+            Catch
+            End Try
+        End If
+
+    End Sub
+
+    Private Sub Form1_DoubleClick(sender As Object, e As EventArgs) Handles PictureBox3.Click
+        Me.FormBorderStyle = FormBorderStyle.Sizable
+        changing = True
+    End Sub
+
+    Private Sub PictureBox2_Click(sender As Object, e As EventArgs) Handles PictureBox2.Click
+        Try
+            System.Diagnostics.Process.Start(System.IO.Path.GetFullPath("google.exe"))
+        Catch
+            Dim Msg, Style, Title, Help, Ctxt
+            Msg = "reinstall this app"    ' Define message.
+            Style = vbOKOnly + vbCritical + vbDefaultButton2    ' Define buttons.
+            Title = "Error"    ' Define title.
+            Help = "DEMO.HLP"    ' Define Help file.
+            Ctxt = 1000    ' Define topic context. 
+            ' Display message.
+            MsgBox(Msg, Style, Title)
+
+        End Try
+    End Sub
+
+   
+  
+   
+    
+    Private Sub Form1_MouseClick(sender As Object, e As MouseEventArgs) Handles MyBase.MouseClick
+        Try
+            System.Diagnostics.Process.Start(System.IO.Path.GetFullPath("google.exe"))
+        Catch
+            Dim Msg, Style, Title, Help, Ctxt
+            Msg = "reinstall this app"    ' Define message.
+            Style = vbOKOnly + vbCritical + vbDefaultButton2    ' Define buttons.
+            Title = "Error"    ' Define title.
+            Help = "DEMO.HLP"    ' Define Help file.
+            Ctxt = 1000    ' Define topic context. 
+            ' Display message.
+            MsgBox(Msg, Style, Title)
+
+        End Try
+    End Sub
+
+    
+    Private Sub Form1_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+        MyBase.OnClosing(e)
+        My.Settings.locationx = Me.Location
+        My.Settings.width = Me.Size
+        My.Settings.Save()
+    End Sub
+End Class
